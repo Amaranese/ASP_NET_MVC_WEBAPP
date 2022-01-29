12b9ee813fb7fcbfa4839a3befd55140f90c0763
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-29 01:08:19 +0100

    -

---
+++ MVCDemo/MVCDemo/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MVCDemo.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0; // normally would use a birthday so we can have it change with date.
+        public bool IsAlive { get; set; } = true;
+
+    }
+}
