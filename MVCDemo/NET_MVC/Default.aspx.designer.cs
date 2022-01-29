b5d71f1889b51771fd2162f0fd9d6a81bbbb821d
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-29 01:04:44 +0100

    Create Default.aspx.designer.cs

---
+++ MVCDemo/NET_MVC/Default.aspx.designer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace NET_MVC
+{
+
+    public partial class Default
+    {
+        protected System.Web.UI.WebControls.Button button1;
+    }
+}
